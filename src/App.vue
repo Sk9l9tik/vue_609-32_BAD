0dc4af83472787af0b2bfe34131c6e7e3c93ff80
Sk9l9tik <adbelosheev@gmail.com>
2026-09-04 19:06:16 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MyComponent from "./components/MyComponent.vue";
+import MyNewComponent from "./components/MyNewComponent.vue";
+</script>
+
+<template>
+  <MyComponent/>
+  <MyNewComponent/>
+</template>
+
+<style scoped></style>
