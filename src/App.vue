8a353c5ebed1644165fd08352a0cc7f52201f273
Sk9l9tik <adbelosheev@gmail.com>
2026-09-11 12:24:32 +0300

    Add assets, update Dockerfile

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@ import MyNewComponent from "./components/MyNewComponent.vue";
 </script>
 
 <template>
-  <MyComponent/>
-  <MyNewComponent/>
+  <!-- <MyComponent/> -->
+  <!-- <MyNewComponent/> -->
+  <h1 class="text-3xl font-bold underline">Hello, Seaman!</h1>
 </template>
 
 <style scoped></style>
